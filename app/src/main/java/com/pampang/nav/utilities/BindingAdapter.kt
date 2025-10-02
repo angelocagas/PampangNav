36b1efb5cc8cf12edbac769511450520975f08a3
Angelo Cagas <gelocagas11@gmail.com>
2025-10-02 11:19:17 +0800

    initial commit

---
+++ app/src/main/java/com/pampang/nav/utilities/BindingAdapter.kt
@@ -0,0 +1,8 @@
+package com.pampang.nav.utilities
+
+
+//@BindingAdapter("items")
+//fun setItems(recyclerView: RecyclerView, items: List<DataModel>?) {
+//    val adapter = recyclerView.adapter as? ItemAdapter
+//    adapter?.submitList(items)
+//}
