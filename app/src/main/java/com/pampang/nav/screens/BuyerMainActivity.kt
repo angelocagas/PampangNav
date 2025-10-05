ba3a4719253321cd5c659bd414663f4d561384c3
Angelo Cagas <gelocagas11@gmail.com>
2025-10-05 21:55:51 +0800

    added buyer and seller activities

---
+++ app/src/main/java/com/pampang/nav/screens/BuyerMainActivity.kt
@@ -0,0 +1,44 @@
+package com.pampang.nav.screens
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.pampang.nav.R
+import com.pampang.nav.databinding.ActivityBuyerMainBinding
+import com.pampang.nav.screens.auth.LoginActivity
+import com.pampang.nav.utilities.extension.launchActivity
+import com.pampang.nav.viewmodels.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlin.getValue
+
+@AndroidEntryPoint
+class BuyerMainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityBuyerMainBinding
+    private val authViewModel: AuthViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initConfig()
+    }
+
+    private fun initConfig() {
+        initBinding()
+        initEventListener()
+    }
+
+    private fun initBinding() {
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_buyer_main)
+        mBinding.lifecycleOwner = this
+    }
+
+    private fun initEventListener() {
+        mBinding.apply {
+            buttonLogout.setOnClickListener {
+                authViewModel.logout()
+                launchActivity<LoginActivity>()
+                finish()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/pampang/nav/screens/SellerMainActivity.kt
@@ -0,0 +1,45 @@
+package com.pampang.nav.screens
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.pampang.nav.R
+import com.pampang.nav.databinding.ActivityBuyerMainBinding
+import com.pampang.nav.databinding.ActivitySellerMainBinding
+import com.pampang.nav.screens.auth.LoginActivity
+import com.pampang.nav.utilities.extension.launchActivity
+import com.pampang.nav.viewmodels.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SellerMainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivitySellerMainBinding
+    private val authViewModel: AuthViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initConfig()
+    }
+
+    private fun initConfig() {
+        initBinding()
+        initEventListener()
+    }
+
+    private fun initBinding() {
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_seller_main)
+        mBinding.lifecycleOwner = this
+    }
+
+    private fun initEventListener() {
+        mBinding.apply {
+            buttonLogout.setOnClickListener {
+                authViewModel.logout()
+                launchActivity<LoginActivity>()
+                finish()
+            }
+        }
+    }
+}
